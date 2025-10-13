335589a0852697d8d3be062134bedfd0e76f3565
Bruno Peres <brunowebdeveloper33@gmail.com>
2025-10-13 20:58:53 +0300

    Fix: Page reload

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Lobby from './pages/Lobby';
 import GameBoard from './pages/GameBoard';
 // ...existing code...
